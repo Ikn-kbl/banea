f22be3fc1fe882c1124660770c951ec764eaf876
ikn slh <salahikene90@gmail.com>
2026-02-17 13:35:18 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,41 +9,45 @@ let users = {};
 
 io.on('connection', (socket) => {
     socket.on('join-room', (data) => {
+        // Avatar 3D par défaut selon le sexe si pas de photo
+        const avatarStyle = data.gender === 'Homme' ? 'bottts' : 'avataaars';
         users[socket.id] = { 
             id: socket.id, 
             name: data.username, 
             age: data.age,
             gender: data.gender,
             peerId: data.peerId, 
-            avatar: `https://api.dicebear.com/7.x/avataaars/svg?seed=${data.username}` 
+            avatar: data.photo || `https://api.dicebear.com/7.x/${avatarStyle}/svg?seed=${data.username}`,
+            blockedUsers: []
         };
         io.emit('update-users', Object.values(users));
     });
 
     socket.on('send-private-message', (data) => {
+        const dest = users[data.toSocketId];
+        if (dest && dest.blockedUsers.includes(socket.id)) return; // Bloqué
+
         const msg = { 
-            fromId: socket.id, // ID de l'envoyeur (indispensable pour le filtrage)
+            fromId: socket.id, 
             sender: users[socket.id].name, 
             text: data.text, 
+            type: data.type || 'text', // text, image, ou voice
             time: new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}) 
         };
-        
-        // On envoie UNIQUEMENT au destinataire ciblé
         io.to(data.toSocketId).emit('receive-private-message', msg);
-        // On renvoie à l'expéditeur pour confirmation
         socket.emit('receive-private-message', msg);
+    });
+
+    socket.on('accept-chat', (data) => {
+        io.to(data.fromId).emit('chat-accepted', { by: socket.id });
+    });
+
+    socket.on('block-user', (data) => {
+        if(users[socket.id]) users[socket.id].blockedUsers.push(data.targetId);
     });
 
     socket.on('end-call', (data) => {
         io.to(data.toSocketId).emit('call-ended-by-peer');
-    });
-
-    socket.on('propose-call', (data) => {
-        io.to(data.toSocketId).emit('incoming-call-request', { 
-            fromName: data.fromName, 
-            fromPeerId: data.fromPeerId,
-            fromSocketId: socket.id
-        });
     });
 
     socket.on('disconnect', () => {
@@ -53,4 +57,4 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 8080;
-http.listen(PORT, () => console.log('BANËA V90 - Private Security Enabled'));
+http.listen(PORT, () => console.log('BANËA V110 - Social & Media Enabled'));
