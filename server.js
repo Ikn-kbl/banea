c43dea831de8ea62c8e1175584ff319655f90571
ikn slh <salahikene90@gmail.com>
2026-02-17 12:58:00 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,27 +10,33 @@ let users = {};
 
 io.on('connection', (socket) => {
     socket.on('join-room', (data) => {
+        // Stockage du profil complet
         users[socket.id] = { 
             id: socket.id, 
             name: data.username, 
+            age: data.age,
+            gender: data.gender,
             peerId: data.peerId, 
-            avatar: `https://api.dicebear.com/7.x/avataaars/svg?seed=${data.username}` 
+            avatar: `https://api.dicebear.com/7.x/avataaars/svg?seed=${data.username}${data.age}` 
         };
         io.emit('update-users', Object.values(users));
     });
 
-    socket.on('typing', (data) => {
-        socket.to(data.to).emit('is-typing', { from: socket.id });
-    });
-
     socket.on('send-private-message', (data) => {
-        const msg = { sender: users[socket.id].name, text: data.text, time: new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}) };
+        const msg = { 
+            sender: users[socket.id].name, 
+            text: data.text, 
+            time: new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}) 
+        };
         io.to(data.toSocketId).emit('receive-private-message', { ...msg, fromId: socket.id });
         socket.emit('receive-private-message', { ...msg, fromId: socket.id });
     });
 
     socket.on('propose-call', (data) => {
-        io.to(data.toSocketId).emit('incoming-call-request', { fromName: data.fromName, fromPeerId: data.fromPeerId });
+        io.to(data.toSocketId).emit('incoming-call-request', { 
+            fromName: data.fromName, 
+            fromPeerId: data.fromPeerId 
+        });
     });
 
     socket.on('disconnect', () => {
@@ -40,4 +46,4 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 8080;
-http.listen(PORT, () => console.log('Expert Server Ready'));
+http.listen(PORT, () => console.log('Server Profile Ready'));
