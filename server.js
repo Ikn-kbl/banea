4474e75db139526e912b8175f2c7b02ab5b1b267
ikn slh <salahikene90@gmail.com>
2026-02-17 13:48:15 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,52 +9,37 @@ let users = {};
 
 io.on('connection', (socket) => {
     socket.on('join-room', (data) => {
-        // Avatar 3D par défaut selon le sexe si pas de photo
-        const avatarStyle = data.gender === 'Homme' ? 'bottts' : 'avataaars';
         users[socket.id] = { 
             id: socket.id, 
             name: data.username, 
             age: data.age,
             gender: data.gender,
             peerId: data.peerId, 
-            avatar: data.photo || `https://api.dicebear.com/7.x/${avatarStyle}/svg?seed=${data.username}`,
-            blockedUsers: []
+            avatar: data.photo || `https://api.dicebear.com/7.x/${data.gender === 'Homme' ? 'bottts' : 'avataaars'}/svg?seed=${data.username}`,
+            likes: 0,
+            blockedBy: []
         };
         io.emit('update-users', Object.values(users));
     });
 
-    socket.on('send-private-message', (data) => {
-        const dest = users[data.toSocketId];
-        if (dest && dest.blockedUsers.includes(socket.id)) return; // Bloqué
+    socket.on('send-like', (targetId) => {
+        if (users[targetId]) {
+            users[targetId].likes++;
+            io.emit('update-users', Object.values(users));
+        }
+    });
 
-        const msg = { 
-            fromId: socket.id, 
-            sender: users[socket.id].name, 
-            text: data.text, 
-            type: data.type || 'text', // text, image, ou voice
-            time: new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}) 
-        };
+    socket.on('send-private-message', (data) => {
+        const msg = { fromId: socket.id, text: data.text, type: data.type, time: new Date().toLocaleTimeString() };
         io.to(data.toSocketId).emit('receive-private-message', msg);
         socket.emit('receive-private-message', msg);
     });
 
-    socket.on('accept-chat', (data) => {
-        io.to(data.fromId).emit('chat-accepted', { by: socket.id });
+    socket.on('propose-call', (data) => {
+        io.to(data.toSocketId).emit('incoming-call', { fromId: socket.id, fromPeerId: data.fromPeerId });
     });
 
-    socket.on('block-user', (data) => {
-        if(users[socket.id]) users[socket.id].blockedUsers.push(data.targetId);
-    });
-
-    socket.on('end-call', (data) => {
-        io.to(data.toSocketId).emit('call-ended-by-peer');
-    });
-
-    socket.on('disconnect', () => {
-        delete users[socket.id];
-        io.emit('update-users', Object.values(users));
-    });
+    socket.on('disconnect', () => { delete users[socket.id]; io.emit('update-users', Object.values(users)); });
 });
 
-const PORT = process.env.PORT || 8080;
-http.listen(PORT, () => console.log('BANËA V110 - Social & Media Enabled'));
+http.listen(8080, () => console.log("V120 - Logic Optimized"));
