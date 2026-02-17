d1c30b397597e7c1b515454eb290e05fa553ab95
ikn slh <salahikene90@gmail.com>
2026-02-17 17:39:21 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,43 +8,31 @@ app.use(express.static(__dirname));
 let users = {};
 
 io.on('connection', (socket) => {
-    console.log('Nouveau visiteur:', socket.id);
-
     socket.on('join-room', (data) => {
-        const defaultAvatar = `https://api.dicebear.com/7.x/${data.gender === 'Homme' ? 'bottts' : 'avataaars'}/svg?seed=${data.username}`;
-        
         users[socket.id] = { 
             id: socket.id, 
             name: data.username, 
             age: data.age,
             gender: data.gender,
             peerId: data.peerId, 
-            avatar: data.photo || defaultAvatar
+            avatar: data.photo || `https://api.dicebear.com/7.x/avataaars/svg?seed=${data.username}`,
+            likes: 0
         };
-        
-        console.log(data.username + " a rejoint la session.");
-        // On renvoie la liste à TOUT LE MONDE
         io.emit('update-users', Object.values(users));
     });
 
+    socket.on('send-like', (id) => {
+        if(users[id]) { users[id].likes++; io.emit('update-users', Object.values(users)); }
+    });
+
     socket.on('send-private-message', (data) => {
-        const msg = { 
-            fromId: socket.id, 
-            sender: users[socket.id] ? users[socket.id].name : "Anonyme", 
-            text: data.text, 
-            type: data.type, 
-            time: new Date().toLocaleTimeString() 
-        };
+        const msg = { fromId: socket.id, sender: users[socket.id].name, text: data.text, type: data.type };
         io.to(data.toSocketId).emit('receive-private-message', msg);
         socket.emit('receive-private-message', msg);
     });
 
-    socket.on('disconnect', () => {
-        if(users[socket.id]) console.log(users[socket.id].name + " est parti.");
-        delete users[socket.id];
-        io.emit('update-users', Object.values(users));
-    });
+    socket.on('disconnect', () => { delete users[socket.id]; io.emit('update-users', Object.values(users)); });
 });
 
-const PORT = process.env.PORT || 8080;
-http.listen(PORT, () => console.log('SERVEUR BANËA ACTIF SUR PORT ' + PORT));
+http.listen(8080, () => console.log('BANËA - IZAN EDITION READY'));
+
