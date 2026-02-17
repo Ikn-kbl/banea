d0171fd0a5ec3675c88666eae0b77cc64e4add64
ikn slh <salahikene90@gmail.com>
2026-02-17 18:09:54 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,23 +9,32 @@ let users = {};
 
 io.on('connection', (socket) => {
     socket.on('join-room', (data) => {
+        // On crée un profil persistant
         users[socket.id] = { 
             id: socket.id, 
+            uid: data.uid, // ID unique permanent
             name: data.username, 
-            age: data.age,
-            peerId: data.peerId, 
-            avatar: data.photo || `https://api.dicebear.com/7.x/big-smile/svg?seed=${data.username}`
+            avatar: data.photo || `https://api.dicebear.com/7.x/avataaars/svg?seed=${data.username}`,
+            peerId: data.peerId
         };
         io.emit('update-users', Object.values(users));
     });
 
-    socket.on('send-private-message', (data) => {
-        const msg = { fromId: socket.id, sender: users[socket.id].name, text: data.text };
-        io.to(data.toSocketId).emit('receive-private-message', msg);
-        socket.emit('receive-private-message', msg);
+    socket.on('send-message', (data) => {
+        const payload = {
+            fromUid: data.fromUid,
+            text: data.text,
+            type: data.type, // 'text' ou 'image'
+            time: new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})
+        };
+        io.to(data.toSocketId).emit('receive-message', payload);
+        socket.emit('receive-message', payload); 
     });
 
-    socket.on('disconnect', () => { delete users[socket.id]; io.emit('update-users', Object.values(users)); });
+    socket.on('disconnect', () => {
+        delete users[socket.id];
+        io.emit('update-users', Object.values(users));
+    });
 });
 
-http.listen(8080, () => console.log('ENGINE ONLINE'));
+http.listen(8080, () => console.log('BANËA SOCIAL HUB - ACTIVE'));
