aac32957227f8a4c89501435bc03cf419c96ea04
ikn slh <salahikene90@gmail.com>
2026-02-17 18:14:29 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,40 +1,24 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(http, { cors: { origin: "*" } });
 
-app.use(express.static(__dirname));
-
-let users = {};
+const users = new Map();
 
 io.on('connection', (socket) => {
-    socket.on('join-room', (data) => {
-        // On crée un profil persistant
-        users[socket.id] = { 
-            id: socket.id, 
-            uid: data.uid, // ID unique permanent
-            name: data.username, 
-            avatar: data.photo || `https://api.dicebear.com/7.x/avataaars/svg?seed=${data.username}`,
-            peerId: data.peerId
-        };
-        io.emit('update-users', Object.values(users));
+    socket.on('join', (user) => {
+        users.set(socket.id, { ...user, id: socket.id });
+        io.emit('roster', Array.from(users.values()));
     });
 
-    socket.on('send-message', (data) => {
-        const payload = {
-            fromUid: data.fromUid,
-            text: data.text,
-            type: data.type, // 'text' ou 'image'
-            time: new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})
-        };
-        io.to(data.toSocketId).emit('receive-message', payload);
-        socket.emit('receive-message', payload); 
+    socket.on('signal', (data) => {
+        io.to(data.to).emit('signal', { from: socket.id, signal: data.signal });
     });
 
     socket.on('disconnect', () => {
-        delete users[socket.id];
-        io.emit('update-users', Object.values(users));
+        users.delete(socket.id);
+        io.emit('roster', Array.from(users.values()));
     });
 });
 
-http.listen(8080, () => console.log('BANËA SOCIAL HUB - ACTIVE'));
+http.listen(8080, () => console.log('CORE ENGINE ACTIVE'));
